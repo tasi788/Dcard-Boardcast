3618b939bd19c1735bbbe9d5af8a11306c177609
DingChen-Tsai <tdc@tdccc.com.tw>
2019-03-23 00:30:19 +0800

    upload

---
+++ main.py
@@ -0,0 +1,87 @@
+import os
+from time import sleep
+from html import escape
+
+import redis
+import requests
+import telegram
+import telegram.error
+
+bot = telegram.Bot(os.environ.get(
+    'token', None))
+client = redis.from_url('redis://redis:6379')
+channel_id = os.environ.get('channel_id', None)
+tdc = os.environ.get('tdc', None)
+
+
+def fetch():
+    url = 'https://www.dcard.tw/_api/forums/sex/posts?popular=false&limit=80'
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        return False
+    else:
+        try:
+            r.json()
+            return r.json()
+        except:
+            return False
+
+
+def boardcast():
+    '''
+    [pic]
+    title(url)
+    body(text)
+    '''
+    fresh = fetch()
+    if fresh == None:
+        return
+    for x in fresh:
+
+        try:
+            result = client.lrange('dcard', 0, -1)
+            post_id = x['id']
+            if str(post_id).encode() not in result:
+                body = x['excerpt']
+                captions = f'<a href="https://www.dcard.tw/f/sex/p/{post_id}">{escape(x["title"])}</a>\n' \
+                           f'#{post_id}\n' \
+                           f'{escape(body)}\n'
+                if x['media']:
+                    if len(x['media']) >= 2:
+                        # telegram.InputMediaPhoto
+                        photo_url_list = list(x['url'] for x in x['media'])
+                        send_list, tmp_list = [], []
+                        for x in photo_url_list:
+                            if len(tmp_list) >= 9:
+                                send_list.append(tmp_list)
+                                tmp_list = []
+                            else:
+                                tmp_list.append(telegram.InputMediaPhoto(x))
+                        if send_list == []:
+                            send_list = [tmp_list]
+                        for y in send_list:
+                            send = bot.send_media_group(channel_id, y)
+                        bot.send_message(
+                            channel_id, captions, parse_mode='html', disable_web_page_preview=True, reply_to_message_id=send[-1].message_id)
+
+                    else:
+                        bot.send_photo(channel_id, x['media'][0]['url'], caption=captions,
+                                       parse_mode='html', disable_web_page_preview=True)
+                else:
+                    bot.send_message(
+                        channel_id, captions, parse_mode='html', disable_web_page_preview=True)
+        except telegram.error.TimedOut:
+            sleep(120)
+        except Exception as e:
+            # raise
+            bot.send_message(tdc, str(e))
+        else:
+            # pass
+            client.lpush('dcard', post_id)
+
+
+while True:
+    boardcast()
+    sleep(300)
